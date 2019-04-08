4d8e678095a7b1dc24e26dc570c2554146d80d62
Murilo Zanini de Carvalho <murilo@Mac-mini-de-Murilo.local>
2019-04-08 07:46:14 -0300

    Solução do problema de pressão do Pneu.

--- Problema_CalibrarPneu/main.cpp
+++ Problema_CalibrarPneu/main.cpp
@@ -3,5 +3,8 @@
 using namespace std;
 
 int main(){
-    
+    int pressao_entrada, pressao_saida;
+    cin >> pressao_entrada;
+    cin >> pressao_saida;
+    cout << (pressao_saida - pressao_entrada) << endl;
 }
